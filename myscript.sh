7e1b35e12dfdc5c3565edd1dcac61eb09cc36d15
anchalawal <anchalawal@in.ibm.com>
2017-07-05 02:43:12 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,19 +1,27 @@
 #!/bin/bash +x
 
+check_and_print()
+{
+  echo "# MY_PARAMETER_CHOICE is set to '${1}'"
+}
+
 echo "##### This is a demo jenkins workshop"
 
 echo "# JAVA_HOME is set to '${JAVA_HOME}'"
 
-case ${MY_PARAMETER} in
+echo "# MY_PARAMETER_STRING is set to '${MY_PARAMETER_STRING}'"
+
+case ${MY_PARAMETER_CHOICE} in
 test) check_and_print test
 ;;
 develop) check_and_print develop
 ;;
 master) check_and_print master
 ;;
-*) echo "# Invalid parameter value ''. Exiting."
+*) echo "# Invalid MY_PARAMETER_CHOICE value '${MY_PARAMETER_CHOICE}'. Exiting."
+   echo "# demo error"
    exit 1
 ;;
 esac
 
-echo "# demo complete"
+echo "# demo success"
